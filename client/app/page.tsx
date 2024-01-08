f8d8e2368628cea082393295031a1c3924eaff1e
juaniwk3 <juaniwk3@gmail.com>
2024-01-08 18:32:55 -0300

    Remove unused async function getData()

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,22 +1,7 @@
 import { History } from "./components/History";
 import { Messages } from "./components/Messages";
 
-async function getData() {
-  const res = await fetch("https://dog-alert-psi.vercel.app/api", {
-    cache: "no-cache",
-  });
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 export default async function Page() {
-  const data = await getData();
-
   return (
     <main>
       <Messages />
