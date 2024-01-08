e5e086e92f94f4bd2de26d6b483a8edd90f8cc47
juaniwk3 <juaniwk3@gmail.com>
2024-01-08 18:45:34 -0300

    Refactor History component and update global CSS
    This commit refactors the History component in the client app and updates the global CSS file. The changes include formatting the motion detection timestamp and adjusting the color scheme.

--- client/app/components/History.tsx
+++ client/app/components/History.tsx
@@ -5,11 +5,14 @@ export const History = async () => {
   const data: MotionsRecord[] = (await getHistory()).data;
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center mt-20 gap-4">
       {data.map((motion) => {
         return (
           <div key={motion.id}>
-            <p>Motion Detected at {motion.createdAt.toLocaleString()}</p>
+            <p className="text-xl">
+              Motion Detected at {motion.createdAt.toString().slice(0, 10)}{" "}
+              {motion.createdAt.toString().slice(11, 19)}
+            </p>
           </div>
         );
       })}
